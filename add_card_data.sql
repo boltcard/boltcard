7e75bc6f810d3cddca3c354da646cc37acc95ddc
Peter Rounce <peter@rounce.me.uk>
2022-08-09 14:46:57 +0000

    add comments to SQL

--- add_card_data.sql
+++ add_card_data.sql
@@ -1,16 +1,19 @@
 \c card_db
 
 INSERT INTO cards (
-		aes_cmac,
-		uid,
-		last_counter_value,
-		lnurlw_request_timeout_sec,
-		enable_flag,
-		tx_limit_sats,
-		day_limit_sats,
-		card_description
+		lock_key,	/* this is key 0 on the card */
+		aes_cmac,	/* this is key 2 on the card */
+		uid,		/* this can be discovered from the service log */
+		last_counter_value,	/* can start at zero and will be updated on first use (before issue) */
+		lnurlw_request_timeout_sec, /* 60 seconds by default */
+		enable_flag,		/* useful for quickly switching card hosting on/off */
+		tx_limit_sats,		/* set at a reasonable value for small test payments in 2022 */
+		day_limit_sats,		/* set at a reasonable value for small test payments in 2022 */
+		card_description,	/* to store a human readable card description (optional) */
+                one_time_code_used	/* used to indicate if the one_time_code for card creation is live */
 )
 	VALUES (
+		'00000000000000000000000000000000',
 		'00000000000000000000000000000000',
 		'00000000000000',
 		0,
@@ -18,5 +21,6 @@ INSERT INTO cards (
 		'Y',
 		1000,
 		10000,
-		'bolt card'
+		'bolt card',
+		'Y'
 	);
