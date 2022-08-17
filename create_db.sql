9799249f6cd818bf66cb1f5dc6edfe8e8652d454
Orfeas Theofanis <orfeas@lstech.io>
2022-08-17 17:21:25 +0300

    change drop&create db to create if not exists

--- create_db.sql
+++ create_db.sql
@@ -1,5 +1,4 @@
-DROP DATABASE IF EXISTS card_db;
-CREATE DATABASE card_db;
+SELECT 'CREATE DATABASE card_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'card_db');
 
 CREATE USER cardapp WITH PASSWORD 'database_password';
 
