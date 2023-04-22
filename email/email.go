d3439db85b70c6432ffd07b08e8889c2dbd36313
Sonoko Mizuki <sonoko@mizuki.io>
2023-04-22 16:27:45 +0900

    Set defualt value of AWS_REGION

--- email/email.go
+++ email/email.go
@@ -93,9 +93,6 @@ func Send_email(recipient string, subject string, htmlBody string, textBody stri
 	aws_ses_secret := db.Get_setting("AWS_SES_SECRET")
 	sender := db.Get_setting("AWS_SES_EMAIL_FROM")
 	region := db.Get_setting("AWS_REGION")
-	if region == "" {
-		region = "us-east-1"
-	}
 
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String(region),
